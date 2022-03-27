06767fec8a6da605852ae3434415e2dcd329d49f
Tarik GOURRAM <tarik.gourram@viseo.com>
2022-03-27 20:23:41 +0200

    add featured Info

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Topbar />
       <div className="container">
         <Sidebar />
-        <div style={{ flex: 4, backgroundColor: "red" }}>doother pages</div>
+        <Home />
       </div>
     </div>
   );
